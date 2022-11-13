01238aad7e1eb2053f1460b69e98694b914042d4
Michael Duisenberg <mrduise@gmail.com>
2022-11-12 23:35:05 -0700

    minor updates
    moved gitignore file to server folder, added more boiler plate code

---
+++ server/src/models/users/users.model.js
@@ -0,0 +1,20 @@
+
+
+/**
+ * takes the users id and returns the user object
+ *
+ * @param {*} id - the users id
+ */
+const getUserById = async (id) => {
+
+}
+
+/**
+ * Takes the user id and returns the users contacts array
+ *
+ * @param {*} id - the users id
+ */
+const getUserContacts = async (id) => {
+    
+}
+
