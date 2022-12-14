84752b5214e5f7c6acbe3873c5778d360700008d
Michael Duisenberg <mrduise@gmail.com>
2022-12-14 12:01:29 -0700

    updated tests
    tests semi work

--- server/src/routes/users/users.test.js
+++ server/src/routes/users/users.test.js
@@ -1,6 +1,11 @@
 const request = require('supertest');
-const app = require('../../app');
+//const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+
+import { jest } from '@jest/globals'
+
+const createUser = jest.fn()
+const app = makeApp({createUser})
 
 const apiRoute = 'http://localhost:8000/api/v1';
 
@@ -8,11 +13,12 @@ describe('Users API', () => {
 
 
   beforeAll(async () => {
-    await mongoConnect();
+    createUser.mockReset()
+    //await mongoConnect();
   });
 
   afterAll(async () => {
-    await mongoDisconnect();
+    //await mongoDisconnect();
   });
 
   const validRegisterUser = {
@@ -24,14 +30,15 @@ describe('Users API', () => {
   };
 
   describe('POST /users/register', () => {
-    it('should register a new user', async () => {
+    test('should register a new user', async () => {
       const res = await request(app)
-        .post(`${apiRoute}/users/register`)
+        .post(`/v1/users/register`)
         .send(validRegisterUser)
         .expect('Content-Type', /json/)
         .expect(201);
 
       expect(res.body).toEqual({
+        __v: 0,
         _id: expect.any(String),
         userName: 'JohnDoe',
         firstName: 'John',
@@ -40,14 +47,17 @@ describe('Users API', () => {
         email: 'johndoe@yahoo.com',
         conversationLog: [],
         contacts: [],
+        blockerUsers: [],
+        createdAt: expect.any(new Date()),
+        updatedAt: expect.any(new Date()),
       });
 
       expect(res.body.password).not.toBe('password');
     });
 
-    it('should return 400 if userName is missing', async () => {
+    test('should return 400 if userName is missing', async () => {
       const res = await request(app)
-        .post('/users/register')
+        .post('/v1/users/register')
         .send({
           firstName: 'John',
           lastName: 'Doe',
