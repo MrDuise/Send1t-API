764d25cc6f31464b0edf580db0c76757f77bbc07
Michael Duisenberg <mrduise@gmail.com>
2022-12-14 19:52:42 -0700

    updated mongodb connection for testing
    updated test cases

--- server/src/routes/users/users.test.js
+++ server/src/routes/users/users.test.js
@@ -1,25 +1,27 @@
+
 const request = require('supertest');
-//const app = require('../../app');
-const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
-
-import { jest } from '@jest/globals'
-
-const createUser = jest.fn()
-const app = makeApp({createUser})
+const app = require('../../app');
+const { mongoConnectTEST, mongoDisconnect } = require('../../services/mongo');
+const { User } = require('../../models/users/users.mongo');
+const { isValidObjectId } = require('mongoose');
 
 const apiRoute = 'http://localhost:8000/api/v1';
 
 describe('Users API', () => {
+  let connection;
 
-
+  let collection;
   beforeAll(async () => {
-    createUser.mockReset()
-    //await mongoConnect();
+    await mongoConnectTEST();
+    
   });
 
   afterAll(async () => {
-    //await mongoDisconnect();
+    //await connection.close();
+    await mongoDisconnect();
   });
+
+ 
 
   const validRegisterUser = {
     userName: 'JohnDoe',
@@ -29,30 +31,48 @@ describe('Users API', () => {
     email: 'johndoe@yahoo.com',
   };
 
+  const validLoginUser = {
+    userName: 'JohnDoe',
+    password: 'password',
+  };
+
+  const invalidLoginUser = {
+    userName: 'JohnDoe',
+    password: 'wrongPassword',
+  };
+
+ 
+
   describe('POST /users/register', () => {
-    test('should register a new user', async () => {
+    it('should register a new user', async () => {
       const res = await request(app)
         .post(`/v1/users/register`)
         .send(validRegisterUser)
         .expect('Content-Type', /json/)
         .expect(201);
 
-      expect(res.body).toEqual({
-        __v: 0,
-        _id: expect.any(String),
-        userName: 'JohnDoe',
-        firstName: 'John',
-        lastName: 'Doe',
-        password: expect.any(String),
-        email: 'johndoe@yahoo.com',
-        conversationLog: [],
-        contacts: [],
-        blockerUsers: [],
-        createdAt: expect.any(new Date()),
-        updatedAt: expect.any(new Date()),
-      });
+      expect(res.body).toHaveProperty('_id');
+      expect(res.body).toHaveProperty('userName');
+      expect(res.body).toHaveProperty('firstName');
+      expect(res.body).toHaveProperty('lastName');
+      expect(res.body).toHaveProperty('email');
+      expect(res.body).toHaveProperty('password');
+      expect(res.body).toHaveProperty('conversationLog');
+      expect(res.body).toHaveProperty('contacts');
+      expect(res.body).toHaveProperty('updatedAt');
+      expect(res.body).toHaveProperty('createdAt');
+      expect(res.body.userName).toBe('JohnDoe');
+      expect(res.body.firstName).toBe('John');
+      expect(res.body.lastName).toBe('Doe');
+      
 
+      const responseDate = new Date(res.body.updatedAt);
+
+      expect(isValidObjectId(res.body._id)).toBe(true);
+      
       expect(res.body.password).not.toBe('password');
+      expect(res.body.password).toBeInstanceOf(String);
+
     });
 
     test('should return 400 if userName is missing', async () => {
@@ -68,7 +88,7 @@ describe('Users API', () => {
         .expect(400);
 
       expect(res.body).toEqual({
-        message: 'userName is required',
+        message: 'Please fill out all fields',
       });
     });
   });
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-const MONGO_URL = process.env.MONGO_URL;
+const MONGO_URL_TEST = process.env.MONGO_URL_TEST;
+const MONGO_URL_PROD = process.env.MONGO_URL_PROD;
 
   
 mongoose.connection.once('open', () => {
@@ -13,15 +14,21 @@ mongoose.connection.on('error', (err) => {
   console.error(err);
 });
 
-async function mongoConnect() {
-  await mongoose.connect(MONGO_URL);
+async function mongoConnectTEST() {
+ await mongoose.connect(MONGO_URL_TEST);
+}
+
+async function mongoConnectPROD() {
+  await mongoose.connect(MONGO_URL_PROD);
 }
 
 async function mongoDisconnect() {
   await mongoose.disconnect();
 }
 
+
 module.exports = {
-  mongoConnect,
+  mongoConnectTEST,
+  mongoConnectPROD,
   mongoDisconnect,
 };
